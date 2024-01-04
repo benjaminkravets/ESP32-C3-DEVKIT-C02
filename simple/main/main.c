61cccda06b0c8969a713317e3da2f6e7019d8031
Benjamin Kravets <bakravets@gmail.com>
2024-01-03 20:54:40 -0500

    file server demo

--- simple/main/main.c
+++ simple/main/main.c
@@ -21,7 +21,6 @@
 #include "esp_tls.h"
 #include "sdkconfig.h"
 
-
 /* A simple example that demonstrates how to create GET and POST
  * handlers and start an HTTPS server.
 */
@@ -32,94 +31,13 @@ static const char *TAG = "example";
 static esp_err_t root_get_handler(httpd_req_t *req)
 {
     httpd_resp_set_type(req, "text/html");
-    httpd_resp_send(req, "<h1>Hello Secure World!</h1>", HTTPD_RESP_USE_STRLEN);
+    httpd_resp_send(req, "<h1>Hello Secure World! <button type=\"button\">Click Me!</button></h1>", HTTPD_RESP_USE_STRLEN);
+
     return ESP_OK;
 }
 
 #if CONFIG_EXAMPLE_ENABLE_HTTPS_USER_CALLBACK
-#ifdef CONFIG_ESP_TLS_USING_MBEDTLS
-static void print_peer_cert_info(const mbedtls_ssl_context *ssl)
-{
-    const mbedtls_x509_crt *cert;
-    const size_t buf_size = 1024;
-    char *buf = calloc(buf_size, sizeof(char));
-    if (buf == NULL) {
-        ESP_LOGE(TAG, "Out of memory - Callback execution failed!");
-        return;
-    }
 
-    // Logging the peer certificate info
-    cert = mbedtls_ssl_get_peer_cert(ssl);
-    if (cert != NULL) {
-        mbedtls_x509_crt_info((char *) buf, buf_size - 1, "    ", cert);
-        ESP_LOGI(TAG, "Peer certificate info:\n%s", buf);
-    } else {
-        ESP_LOGW(TAG, "Could not obtain the peer certificate!");
-    }
-
-    free(buf);
-}
-#endif
-/**
- * Example callback function to get the certificate of connected clients,
- * whenever a new SSL connection is created and closed
- *
- * Can also be used to other information like Socket FD, Connection state, etc.
- *
- * NOTE: This callback will not be able to obtain the client certificate if the
- * following config `Set minimum Certificate Verification mode to Optional` is
- * not enabled (enabled by default in this example).
- *
- * The config option is found here - Component config → ESP-TLS
- *
- */
-static void https_server_user_callback(esp_https_server_user_cb_arg_t *user_cb)
-{
-    ESP_LOGI(TAG, "User callback invoked!");
-#ifdef CONFIG_ESP_TLS_USING_MBEDTLS
-    mbedtls_ssl_context *ssl_ctx = NULL;
-#endif
-    switch(user_cb->user_cb_state) {
-        case HTTPD_SSL_USER_CB_SESS_CREATE:
-            ESP_LOGD(TAG, "At session creation");
-
-            // Logging the socket FD
-            int sockfd = -1;
-            esp_err_t esp_ret;
-            esp_ret = esp_tls_get_conn_sockfd(user_cb->tls, &sockfd);
-            if (esp_ret != ESP_OK) {
-                ESP_LOGE(TAG, "Error in obtaining the sockfd from tls context");
-                break;
-            }
-            ESP_LOGI(TAG, "Socket FD: %d", sockfd);
-#ifdef CONFIG_ESP_TLS_USING_MBEDTLS
-            ssl_ctx = (mbedtls_ssl_context *) esp_tls_get_ssl_context(user_cb->tls);
-            if (ssl_ctx == NULL) {
-                ESP_LOGE(TAG, "Error in obtaining ssl context");
-                break;
-            }
-            // Logging the current ciphersuite
-            ESP_LOGI(TAG, "Current Ciphersuite: %s", mbedtls_ssl_get_ciphersuite(ssl_ctx));
-#endif
-            break;
-
-        case HTTPD_SSL_USER_CB_SESS_CLOSE:
-            ESP_LOGD(TAG, "At session close");
-#ifdef CONFIG_ESP_TLS_USING_MBEDTLS
-            // Logging the peer certificate
-            ssl_ctx = (mbedtls_ssl_context *) esp_tls_get_ssl_context(user_cb->tls);
-            if (ssl_ctx == NULL) {
-                ESP_LOGE(TAG, "Error in obtaining ssl context");
-                break;
-            }
-            print_peer_cert_info(ssl_ctx);
-#endif
-            break;
-        default:
-            ESP_LOGE(TAG, "Illegal state!");
-            return;
-    }
-}
 #endif
 
 static const httpd_uri_t root = {
