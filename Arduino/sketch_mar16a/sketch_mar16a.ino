1e0120b85dcb0811fd52a0d62962db795d21a1d4
Benjamin Kravets <bakravets@gmail.com>
2024-03-19 20:47:49 -0400

    add timer demo

--- Arduino/sketch_mar16a/sketch_mar16a.ino
+++ Arduino/sketch_mar16a/sketch_mar16a.ino
@@ -1,13 +1,31 @@
-// the setup function runs once when you press reset or power the board
+#include <AccelStepper.h>
+
+// define step constant
+#define FULLSTEP 4
+#define STEP_PER_REVOLUTION 2048 // this value is from datasheet
+
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+AccelStepper stepper(FULLSTEP, 0, 1, 2, 3); // ESP32 pin: GPIO19, GPIO18, GPIO17, GPIO16
+
 void setup() {
-  // initialize digital pin GPIO18 as an output.
-  pinMode(18, OUTPUT);
+  //Serial.begin(9600);
+  stepper.setMaxSpeed(1000.0);   // set the maximum speed
+  stepper.setAcceleration(50.0); // set acceleration
+  stepper.setSpeed(1000);         // set initial speed
+  stepper.setCurrentPosition(0); // set position
+  stepper.moveTo(STEP_PER_REVOLUTION); // set target position: 64 steps <=> one revolution
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(18, HIGH); // turn the LED on
-  delay(500);             // wait for 500 milliseconds
-  digitalWrite(18, LOW);  // turn the LED off
-  delay(500);             // wait for 500 milliseconds
+  // change direction once the motor reaches target position
+  if (stepper.distanceToGo() == 0)
+    stepper.moveTo(-stepper.currentPosition());
+
+  stepper.run(); // MUST be called in loop() function
+  
+  Serial.print(F("Current Position: "));
+  Serial.println(stepper.currentPosition());
 }
+
+
+
