df98809641f614989420fc13a6cd4d598a7d985d
Benjamin Kravets <bakravets@gmail.com>
2024-03-20 21:23:05 -0400

    add blink timer demo

---
+++ blink_timer/main/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "led_strip.h"
+#include "esp_log.h"
+
+static const char *TAG = "example";
+
+#define BLINK_GPIO 8
+
+static uint8_t s_led_state = 0;
+
+static led_strip_handle_t led_strip;
+
+static void blink_led(void)
+{
+    /* If the addressable LED is enabled */
+    if (s_led_state) {
+        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        /* Refresh the strip to send data */
+        led_strip_refresh(led_strip);
+    } else {
+        /* Set all LED off to clear all pixels */
+        led_strip_clear(led_strip);
+    }
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
+    /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1, // at least one LED on board
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    /* Set all LED off to clear all pixels */
+    led_strip_clear(led_strip);
+}
+
+void app_main(void)
+{
+	configure_led();
+    while (true) {
+        //printf("Hello from app_main!\n");
+        sleep(1);
+        s_led_state = !s_led_state;
+    	blink_led();
+    }
+}
