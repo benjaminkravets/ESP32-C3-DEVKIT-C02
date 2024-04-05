92e94b1e1da579e46aed885bc240085ae1875c81
Benjamin Kravets <bakravets@gmail.com>
2024-04-04 20:19:37 -0400

    clarify naming, add comments

--- Arduino/integrated/integrated.ino
+++ Arduino/integrated/integrated.ino
@@ -3,16 +3,16 @@
 #include <AccelStepper.h>
 
 #define LED_INDICATOR_PIN 18
-#define FULLSTEP 4
+#define INTERFACE 4 // number of pins for servo motor
 #define STEPS_PER_REV 2048
 
 const char* ssid = "Verizon_YD3BVC"; 
 const char* password = "herd-ace9-broke";
 
 AsyncWebServer server(80);
-AccelStepper stepper(FULLSTEP, 3, 1, 2, 0);
+AccelStepper stepper(INTERFACE, 3, 1, 2, 0);
 
-int LED_state = LOW;
+int stepper_position = LOW;
 
 void setup() {
     //Set up serial connection and LED pin
@@ -22,10 +22,11 @@ void setup() {
     stepper.setSpeed(200);
     stepper.setCurrentPosition(0);
     
+    //Set up pins
     pinMode(LED_INDICATOR_PIN, OUTPUT);
-    digitalWrite(LED_INDICATOR_PIN, LED_state);
+    digitalWrite(LED_INDICATOR_PIN, stepper_position);
 
-    //Network Connection
+    //Network Connection, wait in loop until connected
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
@@ -37,28 +38,29 @@ void setup() {
     Serial.println(WiFi.localIP());
     Serial.println(WiFi.SSID());
 
-
-    server.on("/led1/status", HTTP_GET, [](AsyncWebServerRequest* request){
-                              Serial.println("Request Received:");
-                              Serial.println("GET /");
-                              request->send(200, "text/html", ((LED_state == LOW) ? "OFF" : "ON"));
+    // assign response for for status uri, return servo state
+    server.on("/servo/status", HTTP_GET, [](AsyncWebServerRequest* request){
+      Serial.println("Request Received:");
+      Serial.println("GET /");
+      request->send(200, "text/html", (stepper_position == LOW) ? "LOW" : "HIGH");
     });
 
-    
-    server.on("/led1/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    // assign response for state uri, move to high state, return status
+    server.on("/servo/high", HTTP_GET, [](AsyncWebServerRequest *request){
       Serial.println("Request GET /led1/on");
-      stepper.moveTo(STEPS_PER_REV * 3.5);
-      LED_state = HIGH;
-      digitalWrite(LED_INDICATOR_PIN, LED_state);
-      request->send(200, "text/html", "ON");
+      stepper.moveTo(STEPS_PER_REV * 3.5); // move 3.5 revolutions (needed steps = steps per revolution * revolutions)
+      stepper_position = HIGH;
+      digitalWrite(LED_INDICATOR_PIN, stepper_position);
+      request->send(200, "text/html", "HIGH");
     });
 
-    server.on("/led1/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    // assign response for state uri, move to low state, return status
+    server.on("/servo/low", HTTP_GET, [](AsyncWebServerRequest *request){
       Serial.println("Request GET /led1/off");
-      stepper.moveTo(0);
-      LED_state = LOW;
-      digitalWrite(LED_INDICATOR_PIN, LED_state);
-      request->send(200, "text/html", "OFF");
+      stepper.moveTo(0); // move back from high (will move back 3.5 revolutions)
+      stepper_position = LOW;
+      digitalWrite(LED_INDICATOR_PIN, stepper_position);
+      request->send(200, "text/html", "LOW");
     });
 
     server.begin();
